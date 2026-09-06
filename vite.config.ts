40dd873df38ab1fad7d6000d297683f1b1c54878
akash tolanur <akashtolanur171717@gmail.com>
2026-09-06 17:33:24 +0530

    add vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If you're deploying to https://<username>.github.io/<repo-name>/
-// (a project page, not a *.github.io root repo), set base to '/<repo-name>/'.
-// If this repo IS named <username>.github.io, leave base as '/'.
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // <-- change 'portfolio' to your actual repo name
+  base: '/', 
 })
